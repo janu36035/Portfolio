1298f4b768fa35b8ddab474d669d525586c22bb2
Jahnavi Polamreddy <polamreddyjahnavi@gmail.com>
2026-07-24 21:26:51 +0530

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// ==============================
+// ACTIVE NAVIGATION LINK
+// ==============================
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.onscroll = () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") == "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+};
